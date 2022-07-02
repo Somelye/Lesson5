b3827113d44de8d63ebbeb9b2c7b32d84fd6ae5a
Омелянчук <omelynchuks@gmail.com>
2022-07-02 11:14:34 +0300

    Выполнил 2 из 3 заданий

--- src/HomeWork1_1.java
+++ src/HomeWork1_1.java
@@ -7,9 +7,14 @@
 // 2.Создать два класса, которые будут наследоваться от абстрактного класса, и реализовывать особенности этих машин
 // Методы должны просто печатать на экран действия машин (начал движение, включил музыку и тд.)
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HomeWork1_1 {
-        
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Toyota toyota = new Toyota();
         Jiguli jiguli = new Jiguli();
         toyota.turnMusic();
@@ -20,5 +25,19 @@ public class HomeWork1_1 {
         jiguli.start();
         jiguli.stop();
         jiguli.turnLights();
+
+//Задача №2
+// Необходимо:
+// 1. Создать папку resource, пометить ее как папку Resourсe root.
+// 2. Создать в ней файл "my_first_file.txt". На первой строке написать: "Моя бабушка", на второй: "читает газету жизнь"
+// 3. Прочитать файл, и вывести на экран все слова файла в одну строку
+// Ожидаемый результат: "Моя бабушка читает газету жизнь"
+
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Study\\Java\\repos\\HomeWork3\\resource\\my_first_file.txt"));
+        String result = "";
+        while (reader.ready()) {
+            result += reader.readLine() + " ";
+        }
+            System.out.println(result);
     }
 }
